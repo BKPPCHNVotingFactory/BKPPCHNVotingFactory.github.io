6e2361f9736bafb3b969e996b4e76f3979d1406d
xiao_chang <2556940481@qq.com>
2024-01-09 13:43:19 +0800

    upd: new

---
+++ js/statistical-sanook.js
@@ -0,0 +1,109 @@
+$(document).ready(function () {
+  // 获取图片并写入
+  // $("#statistical_img").append(
+  //   "<img src='../json/compareVote.png?temp='" + Math.random() + "'>"
+  // );
+
+  // // 替换为默认图片
+  // $("img").on("error", function () {
+  //   $(this).attr("src", "../json/compareVoteError.png");
+  // });
+
+  // 获取数据
+  let voteArr = [];
+
+  $.ajaxSettings.async = false;
+  $.getJSON(`../json/vote-sanook.json?random=${Math.random()}`, (resJ) => {
+    voteArr = resJ.res;
+  });
+
+  // console.log(voteArr);
+
+  baseDom = $("#statistical_json");
+  for (let i = 0; i < voteArr.length; i++) {
+    let item = voteArr[i];
+    let statisticalListDom = $("<div/>", {
+      class: "statistical_list",
+    }).append(
+      $("<header/>", {
+        html: `${item.date} Vote`,
+      })
+    );
+    for (let j = 0; j < item.data.length; j++) {
+      let item_data = item.data[j];
+
+      let tbodyDom = $("<tbody/>");
+      for (let h = 0; h < item_data.list.length; h++) {
+        let item_list = item_data.list[h];
+        tbodyDom.append(
+          $("<tr/>", {
+            class: item_list.active_item == 1 ? "active_item" : "",
+          }).append(
+            $("<th/>", {
+              scope: "row",
+              html: h + 1,
+            }),
+            $("<td/>", {
+              html: item_list.item_name,
+            }),
+            $("<td/>", {
+              html: item_list.score,
+            }),
+            $("<td/>", {
+              html: item_list.weight,
+            })
+          )
+        );
+      }
+
+      let sectionDim = $("<section/>", {
+        class: "group_header",
+      }).append(
+        $("<div/>", {
+          class: "group_title",
+        }).append(
+          $("<span/>", {
+            html: item_data.category_name,
+          }),
+          $("<span/>", {
+            html: `总票数：${item_data.sumScore} 票`,
+          })
+        ),
+        $("<div/>", {
+          class: "group_vote_list",
+        }).append(
+          $("<table/>", {
+            class: "table table-dark",
+          }).append(
+            $("<thead/>").append(
+              $("<tr/>").append(
+                $("<th/>", {
+                  scope: "col",
+                  html: "排名",
+                }),
+                $("<th/>", {
+                  scope: "col",
+                  html: "姓名",
+                }),
+                $("<th/>", {
+                  scope: "col",
+                  html: "票数",
+                }),
+                $("<th/>", {
+                  scope: "col",
+                  html: "比例",
+                })
+              )
+            ),
+            tbodyDom
+          )
+        )
+      );
+
+      statisticalListDom.append(sectionDim);
+    }
+
+    baseDom.append(statisticalListDom);
+  }
+  // $("#statistical_json").
+});
---
+++ json/get_sanook_json.py
@@ -0,0 +1,103 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+import datetime
+import os
+
+# coding:utf-8
+respostGet = requests.get("https://season.sanook.com/topoftheyear/")
+# print(respostGet.text)
+
+# 解析当前的html
+soup = BeautifulSoup(respostGet.text, 'html.parser')
+
+# 获取当前页面的script
+json_obj = json.loads(soup.find("script", {"id": "__NEXT_DATA__"}).get_text())
+
+pageProps = json_obj['props']['pageProps']
+sumData = pageProps['data']
+
+# with open('b.json', 'w') as fp:
+#     json.dump(json_obj, fp, ensure_ascii=False)
+
+# 艺术家
+votes = []
+for sumItem in sumData:
+    if sumItem["id"] == 12:
+        votes = sumItem
+
+# 数组集合
+groupArr = [{
+    "category_id": 12,
+    "sumScore": 0,
+    "item_id": [ 71 ],
+    "item_score": 0,
+    "category_name": "ศิลปินดาวรุ่งแห่งปี"
+}]
+
+# 组装之后的数组
+resultArr = []
+for groupItem in groupArr:
+    groupObj = {
+        "sumScore": '/',
+        "list": [],
+        "category_id": "",
+        "category_name": ""
+    }
+    for voteItem in votes["choices"]:
+        active_item = 2
+        if groupItem['category_id'] == votes['id']:
+            groupObj['category_id'] = groupItem['category_id']
+            groupObj['category_name'] = groupItem['category_name']
+            
+            for item_vote_id in groupItem['item_id']:
+                if voteItem['id'] == item_vote_id:
+                    active_item = 1
+            groupObj['list'].append({
+                "item_name": voteItem['name'],
+                "score": "/",
+                "active_item": active_item,
+                "weight": voteItem['percentage'],
+                "item_image": "/"
+            })
+    resultArr.append(groupObj)
+
+
+# 转换北京时间 （UTC）
+def format_time(bj_format="%Y-%m-%d %H:%M:%S"):
+    uct_time = datetime.datetime.utcnow()
+    bj_dt = uct_time + datetime.timedelta(hours=8)
+    time_str = bj_dt.strftime(bj_format)
+    return time_str
+
+
+# 获取当前的时间
+currentTime = format_time()
+resultObj = {"date": currentTime, "data": resultArr}
+# print(resultObj)
+
+# 读取 json 文件
+if not os.path.exists("vote-sanook.json"):
+    with open('vote-sanook.json', 'w') as one:
+        json_base = {"res": []}
+        json.dump(json_base, one, ensure_ascii=False)
+
+with open('vote-sanook.json', 'r') as fp:
+    json_data = json.load(fp)
+
+# print(json_data)
+
+# 写入 json 文件
+if not json_data["res"]:
+    tempList = []
+    tempList.append(resultObj)
+    json_data["res"] = tempList
+else:
+    json_data["res"].append(resultObj)
+
+sort_data = sorted(json_data["res"], key=lambda i: i['date'], reverse=True)
+json_data["res"] = sort_data
+
+with open('vote-sanook.json', 'w') as oldDate:
+    json.dump(json_data, oldDate, ensure_ascii=False)
